41edc17809dfc5a2d8ff973ebc0c7a09395a41bc
Ahmed <ahmaat19@gmail.com>
2022-01-27 12:04:01 +0300

    updating footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
         <div className='row'>
           <div className='col text-center py-3'>
             Copyright &copy;{' '}
-            <a href=' https://geeltech.com' target='blank'>
-              https://geeltech.com
+            <a href=' https://websom.dev' target='blank'>
+              websom.dev
             </a>
           </div>
         </div>
